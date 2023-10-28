01d3ddbb62c4300067b09bc94c8cc0fad3cb828f
Juan Carlos Lumbiarres <jclumbiarres@protonmail.com>
2023-10-28 10:12:30 +0200

    Reglas básicas eslint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Todo from './lib';
-
 const todo = new Todo('Arreglar tsconfig.json y package.json');
 console.log(todo.completed); // false
 todo.toggle();
@@ -7,6 +6,5 @@ console.log(todo.completed); // true
 todo.toggle();
 console.log(todo.completed); // false
 todo.toggle();
-
 console.log(todo.completed); // false
 console.log('hola mundo!');
